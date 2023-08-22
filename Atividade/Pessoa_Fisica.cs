45d29916e4c5ace1ef8895b55b2f717efdfeae7b
Kaio-code-creator <kaioasousa12@gmail.com>
2023-08-22 00:00:51 -0300

    .

--- Atividade/Pessoa_Fisica.cs
+++ Atividade/Pessoa_Fisica.cs
@@ -2,7 +2,7 @@ namespace Atividade
 {
     class Pessoa=Fisica :Clientes 
     {
-        public string cpf;
-        public string rg;
+        public string cpf{get; set;}
+        public string rg{get; set;}
     }
 }
--- Atividade/Pessoa_Juridica.cs
+++ Atividade/Pessoa_Juridica.cs
@@ -1,9 +1,9 @@
 namespace Atividade
 {
-    class Pessoa_Juridica
+    class Pessoa_Juridica : Clientes
     {
-        public string cnpj {get; set;}; 
-        public string ie {get; set;};
+        public string cnpj {get; set;}
+        public string ie {get; set;}
 
         public override void Pagar_Imposto(float v)
         {
